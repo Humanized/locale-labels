dbac15353f79732b5a124747ef02d467fec7279e
jeffrico <jeffrey@humanized.be>
2016-09-16 12:52:55 +0200

    [ADD] Language file

--- Language.php
+++ Language.php
@@ -1,1 +1,14 @@
 
+<?php
+namespace humanized/locale;
+
+use Locale;
+
+class Language
+{
+    public static function label($locale,$format){
+    return Locale::locale_get_display_name($locale, $format);  
+    }
+}
+?>
+
